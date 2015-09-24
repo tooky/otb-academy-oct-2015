acfaa38ec3cf8bb738dd95cd1bb4c81640cff859
Michael Hibbs <michael.hibbs@onthebeach.co.uk>
2015-09-24 11:49:17 +0100

    Add basic discounting.

--- harry_potter/spec/harry_potter_spec.rb
+++ harry_potter/spec/harry_potter_spec.rb
@@ -12,13 +12,26 @@ module ArryPotta
     4 => 20,
     5 => 25,
   }
+
+  def self.clamp(v, min, max)
+    (v <= min) ? min : (v >= max) ? max : v
+  end
+
+  def self.get_discount(books)
+    books = clamp(books, 0, DISCOUNTS.size)
+
+    DISCOUNTS[books] * 0.01
+  end
 end
 
 def book_prices(books)
-  # Calculate the base price for all books
-  books.inject(0) do |sum, book_id|
-    sum += ArryPotta::BASE_PRICE
-  end
+  total_price ||= 0
+  unique_books = books.uniq
+
+  discount = ArryPotta::get_discount(unique_books.size)
+  total_price = books.size * ArryPotta::BASE_PRICE
+
+  total_price - (total_price * discount)
 end
 
 RSpec.describe "harry potter" do
@@ -29,5 +42,18 @@ RSpec.describe "harry potter" do
   it "should cost 16 GBP to buy 2 of the same book" do
     # dev;binding.pry
     expect( book_prices([ 1, 1 ]) ).to eq( 16 )
+  end
+
+  it "should cost 15.20 GBP (5%) to buy 2 different books" do
+    expect( book_prices([ 1, 2 ]) ).to eq( 15.20 )
+  end
+
+  it "should cost 21.6 GBP (10%) to buy 3 different books" do
+    expect( book_prices([ 1, 2, 3 ]) ).to eq( 21.6 )
+  end
+
+  it "should cost 51.20 GBP to buy a bunch of books" do
+    # dev;binding.pry
+    expect( book_prices([ 1, 1, 2, 2, 3, 3, 4, 5 ]) ).to eq( 51.20 )
   end
 end
