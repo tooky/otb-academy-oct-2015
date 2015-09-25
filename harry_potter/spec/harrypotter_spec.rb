ed1e686bae4eb222e52fbab425223d94f7207dd5
mori15haru <mori15haru@outlook.com>
2015-09-25 10:44:36 +0100

    modified greey algorithm

--- harry_potter/spec/harrypotter_spec.rb
+++ harry_potter/spec/harrypotter_spec.rb
@@ -32,11 +32,20 @@ class HPspecial
     @@discount = {2=>0.95, 3=>0.9, 4=>0.8, 5=>0.75}
     def initialize(purchase)
         @purchase = purchase
+        @purchase_hash = Hash.new(0)
         @purchase_list = [0, 0, 0, 0, 0, 0]
         @purchase.each do |p|
+            @purchase_hash[p] += 1
             @purchase_list[p] += 1
         end
     end
+    #Defines which method to use to calculate the final price
+    def basket
+        return self.basket_greedy_modified
+    end
+    #basket_greedy
+    #Calculates the final price in a greedy way
+    #Discounts will be applied in an order of discount rates
     def basket_greedy
         run = true
         special_price = 0
@@ -56,8 +65,51 @@ class HPspecial
         end
         special_price
     end
-    def basket
-         
+    #basket_greedy
+    #Basically same as 'basket_greedy', but it prefers 'two 'four-different-books' discount
+    def try_discount(count)
+        if count == 5
+            try_hash = @purchase_hash.clone
+            purchase_sorted = try_hash.sort_by{|ep,n| n}.reverse
+            (0..3).each do |here|
+                try_hash[purchase_sorted[here][0]] -= 1
+            end
+            count = try_hash.values.count{|x| x > 0}
+            if count >= 4
+                purchase_sorted = try_hash.sort_by{|ep,n| n}.reverse
+                (0..3).each do |here|
+                    try_hash[purchase_sorted[here][0]] -= 1
+                end
+                @purchase_hash = try_hash
+                return true
+            end
+        end
+        return false
+    end
+    def basket_greedy_modified
+        run = true
+        special_price = 0
+        while run
+            count = @purchase_hash.values.count{|x| x > 0}
+            if count > 1
+                #two 'four-diff-books' discounts
+                try = self.try_discount(count)
+                if try
+                    special_price += 2*8*4*@@discount[4]
+                else
+                    special_price += 8*count*@@discount[count]
+                    @purchase_hash.each{|k,v| @purchase_hash[k] -= 1 }
+                end        
+            else
+                remains = @purchase_hash.values.inject(:+)
+                special_price += 8*remains if remains
+                run = false
+            end
+        end
+        special_price
+    end
+    def basket_tree
+    
     end
 end
 
