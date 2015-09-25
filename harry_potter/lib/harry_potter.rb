3f7929bf64f4a391efb8a64ab76ae9c77b1611c4
Kevin Mulhern <kevin.mulhern@onthebeach.co.uk>
2015-09-25 13:08:08 +0100

    Refactored and added & passed test for 2 sets of 4

--- harry_potter/lib/harry_potter.rb
+++ harry_potter/lib/harry_potter.rb
@@ -1,29 +1,63 @@
-def book_discount_calculator(book_list)
-	result = 0
-	while book_list.size != 0
-		book_list.delete(0)
-		result += discount(book_list)
-		book_list.map! { |book| book - 1}
+DISCOUNT = { 1 => 1, 2 => 0.95, 3 => 0.9, 4 => 0.8, 5 => 0.75}
+
+
+def book_discount_calculator(books_list)
+	@price = 0
+	return 0 if books_list.empty?
+	@books_list = books_list
+	book_count = @books_list.max
+	puts @books_list
+	puts " book count is #{book_count}"
+	i = 0
+
+	book_count.times do
+		@books_list.delete(0)
+		puts @books_list.inspect
+		if special_case
+			puts "in"
+			@books_list = special_discount
+		else
+			puts "in h"
+			discounter
+		end
+		puts @books_list.inspect
+		i =+ 1
 	end
-	result
+	@price
+end
+
+def discounter
+	puts "in di"
+	puts @books_list.inspect
+	@price += (8 * books_count) * DISCOUNT[books_count]
+	puts "price is #{@price}"
+	@books_list.map! { |book| book -= 1}
 end
 
 
-def discount(book_list)
-	puts "In the discounter"
-	  if book_list.empty?
-	  	0
-	  elsif book_list.size == 1
-	  	8
-	  elsif book_list.size == 2
-	  	15.20
-	  elsif book_list.size == 3
-	  	21.6
-	 elsif book_list.size == 4
-	 	28.8
-	 elsif book_list.size == 5
-	 	30
-	 end
+def books_count
+	@books_list.size
 end
 
-puts book_discount_calculator([2,2,2,1,1])
+
+
+def total_books
+	@books_list.inject(0) { |sum, num| sum += num}
+end
+
+def special_case
+	(total_books % 4) == 0 && (total_books % 5) != 0 && @books_list.size >= 4
+end
+
+def special_discount
+	books_list = @books_list.each_with_index.collect { |book,index| index < 4 ? book -= 1 : book }
+	@price += (4 * 8) * 0.8 
+	return books_list
+end
+puts book_discount_calculator([5, 5, 4, 5, 4])
+
+
+
+
+
+
