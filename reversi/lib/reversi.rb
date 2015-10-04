b17076c2b8dc1711b550c15505801d8f128f2979
Adam Modzelewski <trash@modzelewski.eu>
2015-10-04 23:14:04 +0100

    Refactored Reversi class
    Spllit logic into smaller methods
    Flattern nested if
    Changed directions to be created in a loop instead of being hardcoded

--- reversi/lib/reversi.rb
+++ reversi/lib/reversi.rb
@@ -7,6 +7,7 @@ class Reversi
     draw_moves(all_moves)
   end
 
+  private
   def draw_moves(positions)
     positions.each { |position| @board.set_char('0', position) }
     @board.to_s
@@ -23,29 +24,38 @@ class Reversi
   end
 
   def all_directions
-    [Point.new(-1, -1), Point.new(-1, 0), Point.new(-1, 1),
-    Point.new(0, -1), Point.new(0, 0), Point.new(0, 1),
-    Point.new(1, -1), Point.new(1, 0), Point.new(1, 1)
-    ]
+    direcions = []
+    -1.upto(1) { |x|
+      -1.upto(1) { |y|
+        direcions << Point.new(x, y)
+      }
+    }
+    direcions
   end
 
   def find_legals(position, direction, counter)
     next_position = position + direction
-    object = @board.char_at(position)
-    next_object = @board.char_at(next_position)
-    return nil unless object && next_object
-    if counter == 0
-      return find_legals(next_position, direction, counter + 1)
-    elsif object != oposite_player(@board.turn)
-      return nil
-    else object == oposite_player(@board.turn)
-      if next_object == '.'
-        return next_position
-      else
-        return find_legals(next_position, direction, counter + 1)
-      end
+    char = @board.char_at(position)
+    next_char = @board.char_at(next_position)
+    if first_char?(counter) || (oposite?(char) && oposite?(next_char))
+      find_legals(next_position, direction, counter + 1)
+    elsif oposite?(char) && free?(next_char)
+      next_position
+    else
+      nil
     end
-    nil
+  end
+
+  def first_char?(counter)
+    counter == 0
+  end
+
+  def oposite?(char)
+    char == oposite_player(@board.turn)
+  end
+
+  def free?(char)
+    char == '.'
   end
 
   def oposite_player(player)
